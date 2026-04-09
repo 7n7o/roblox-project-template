a445956dcb65691177f3c828ca03043aa0d3a8b2
7n7o <69775249+7n7o@users.noreply.github.com>
2026-04-09 12:32:33 +0100

    update template src

--- src/init.lua
+++ src/init.lua
@@ -2,7 +2,7 @@ local Promise = require("@pkg/Promise")
 
 local helloPromise = Promise.new(function(res, rej)
     task.wait(1)
-    res("Hello, world!")
+    res("Hello, world! From Template")
 end)
 
 helloPromise:andThen(print)
